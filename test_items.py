7fbfbca23257a25f3e0bfed4f5e6d28b6ab304eb
avzhur <avzhur1@mfu.tsu.ru>
2020-02-09 22:40:31 +0700

    The last version

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_visible(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(3)
     try:
         button = browser.find_element_by_css_selector("button.btn-add-to-basket")
         b = True
